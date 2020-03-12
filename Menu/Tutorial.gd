2441bb4e030c44ed7377a0b009b3991b18414bab
Dev ifsp ptb <dev.ifsp.ptb@gmail.com>
2020-03-12 11:42:16 -0300

    Inicio do tutorial.

---
+++ Menu/Tutorial.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_Tutorial_pressed():
+	get_tree().change_scene("res://Mundos/Tutorial/Tutorial.tscn")
---
+++ Mundos/Tutorial/Voltar.gd
@@ -0,0 +1,6 @@
+extends Button
+
+
+
+func _on_Voltar_pressed():
+	get_tree().change_scene("res://Menu/Menu.tscn")
